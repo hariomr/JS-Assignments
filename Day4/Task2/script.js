c1cbf8cc6dfcc7e61be2a9afee08d5e80127bd07
Hariom Rathore <hariom.rathore@expansionjs.com>
2024-12-26 12:52:54 +0530

    Update script.js

--- Day4/Task2/script.js
+++ Day4/Task2/script.js
@@ -2,44 +2,71 @@ let inputEle = document.querySelector("#add-todo");
 let submitbtn = document.querySelector("#submit-btn");
 let todoList = document.querySelector("#todo");
 
-let todos = [];
+// let todos = [];
 
-function renderTodo() {
-    todoList.innerHTML = ""; 
-    if (todos.length > 0) {
-        todos.forEach((todo, index) => {
-            const todoItem = document.createElement("div");
+// function renderTodo() {
+//   todoList.innerHTML = "";
+//   if (todos.length > 0) {
+//     todos.forEach((todo, index) => {
+//       const todoItem = document.createElement("div");
 
-            const checkBox = document.createElement("input");
-            checkBox.type = "checkbox";
+//       const checkBox = document.createElement("input");
+//       checkBox.type = "checkbox";
 
-            const todoText = document.createTextNode(todo);
+//       const todoText = document.createTextNode(todo);
 
-            const deleteButton = document.createElement("button");
-            deleteButton.innerText = "Delete";
-            deleteButton.onclick = () => deleteTodo(index);
+//       const deleteButton = document.createElement("button");
+//       deleteButton.innerText = "Delete";
+//       deleteButton.onclick = () => deleteTodo(index);
 
-            todoItem.appendChild(checkBox);
-            todoItem.appendChild(todoText);
-            todoItem.appendChild(deleteButton);
+//       todoItem.appendChild(checkBox);
+//       todoItem.appendChild(todoText);
+//       todoItem.appendChild(deleteButton);
 
-            todoList.appendChild(todoItem);
-        });
-    }
-    inputEle.value = ""; 
-}
+//       todoList.appendChild(todoItem);
+//     });
+//   }
+//   inputEle.value = "";
+// }
 
-function deleteTodo(index) {
-    todos.splice(index, 1); 
-    renderTodo(); 
-}
+// function deleteTodo(index) {
+//   todos.splice(index, 1);
+//   renderTodo();
+// }
 
 function addTodo() {
-    const todoText = inputEle.value.trim(); 
-    if (todoText) {
-        todos.push(todoText); 
-        renderTodo(); 
-    }
+
+  const todoList = document.getElementById("todo");
+  const todoval = inputEle.value.trim();
+
+  const todoItem = document.createElement("div");
+
+  const checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  const todoText = document.createTextNode(todoval);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+
+
+  const statusText = document.createElement("span");
+  statusText.textContent = "Incomplete"; 
+  checkBox.addEventListener("change", () => {
+    statusText.textContent = checkBox.checked ? "Completed" : "Incomplete";
+  });
+  
+  todoItem.appendChild(todoText);
+  todoItem.appendChild(checkBox);
+  todoItem.appendChild(statusText);
+  todoItem.appendChild(deleteButton);
+
+  todoList.appendChild(todoItem);
+
+  inputEle.value = "";
+
+  deleteButton.addEventListener("click",()=>{
+    todoList.removeChild(todoItem);
+  })
 }
 
 submitbtn.addEventListener("click", addTodo);
